fe189524404b06c589fc3ebc0585b15a87c6782c
Jannis Lübbe <jannisluebbe1982@gmail.com>
2024-02-12 15:45:50 +0100

    removed unused code.

--- routers/meters.py
+++ routers/meters.py
@@ -1,6 +1,5 @@
 import os
 import json
-import asyncio
 from redis import asyncio as aioredis
 from fastapi import APIRouter, WebSocket
 
@@ -10,20 +9,6 @@ if "REDIS_HOST" in os.environ:
     redis_host = os.environ["REDIS_HOST"]
 else:
     redis_host = "127.0.0.1"
-
-
-async def _get_channel_data(channel):
-    redis_connection = aioredis.Redis(host=redis_host, decode_responses=True)
-    pubsub = redis_connection.pubsub(ignore_subscribe_messages=True)
-    await pubsub.subscribe(channel)
-    while True:
-        message = await pubsub.get_message()
-        if message is not None:
-            _channel = message["channel"]
-            _data = json.loads(message["data"])
-            _data["channel"] = _channel
-            return _data
-        await asyncio.sleep(0.01)
 
 
 @router.websocket("/ws/{channel}")
