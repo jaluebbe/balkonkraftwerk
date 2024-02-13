72045780c4486bca2a1b8aacc57c8aa53b341df0
Jannis Lübbe <jannisluebbe1982@gmail.com>
2024-02-14 00:02:17 +0100

    added review caching functionality.

--- routers/review.py
+++ routers/review.py
@@ -1,9 +1,17 @@
+import os
 from pathlib import Path
+import orjson
 import numpy as np
 import pandas as pd
-from fastapi import APIRouter, Query, HTTPException
+from fastapi import APIRouter, Query, HTTPException, Response
 from fastapi.responses import ORJSONResponse
+from redis import asyncio as aioredis
 from config import max_inverter_limit, battery_inverter_serials
+
+if "REDIS_HOST" in os.environ:
+    redis_host = os.environ["REDIS_HOST"]
+else:
+    redis_host = "127.0.0.1"
 
 router = APIRouter()
 log_directory = Path("../logs_json")
@@ -19,18 +27,8 @@ def _plot_list(power: pd.Series) -> dict[str, list]:
     return {"utc_ms": (_data.index * 1e3).to_list(), "values": _data.to_list()}
 
 
-@router.get("/api/review/day", response_class=ORJSONResponse)
-async def get_day_review(
-    date: str = Query("*", regex="^[0-9]{8}$"),
-):
-    datasets = [_file for _file in log_directory.glob(f"power_*_{date}.json")]
-    if len(datasets) == 0:
-        raise HTTPException(status_code=404, detail="dataset unknown.")
-    elif len(datasets) > 1:
-        raise HTTPException(
-            status_code=409, detail="no exact match for dataset."
-        )
-    df = pd.read_json(datasets[0]).set_index("utc")
+def create_day_review(json_file: Path) -> dict:
+    df = pd.read_json(json_file).set_index("utc")
     response = {}
     response["consumer_power"] = _plot_list(df["consumer_power"])
     response["producer_power"] = _plot_list(df["producer_power"])
@@ -59,4 +57,29 @@ async def get_day_review(
         response["battery_energy"] = _energy_power(
             df["battery_power"], df.index
         )
+    return response
+
+
+@router.get("/api/review/day", response_class=ORJSONResponse)
+async def get_day_review(
+    date: str = Query("*", regex="^[0-9]{8}$"),
+):
+    redis_connection = aioredis.Redis(host=redis_host, decode_responses=True)
+    _key = f"power/review/day/{date}"
+    json_string = await redis_connection.get(_key)
+    if json_string is not None:
+        return Response(content=json_string, media_type="application/json")
+    datasets = [_file for _file in log_directory.glob(f"power_*_{date}.json")]
+    if len(datasets) == 0:
+        raise HTTPException(status_code=404, detail="dataset unknown.")
+    elif len(datasets) > 1:
+        raise HTTPException(
+            status_code=409, detail="no exact match for dataset."
+        )
+    response = create_day_review(datasets[0])
+    await redis_connection.setex(
+        name=_key,
+        time=3600,
+        value=orjson.dumps(response, option=orjson.OPT_SERIALIZE_NUMPY),
+    )
     return ORJSONResponse(response)
