540a67808baf3be5fa32f3176cdf9a44afe48e8c
Jannis Lübbe <jannisluebbe1982@gmail.com>
2024-03-12 11:05:45 +0100

    added class to read Tibber price.

---
+++ tibber_price.py
@@ -0,0 +1,19 @@
+import arrow
+import tibber
+from config import tibber_api_key
+
+
+class tibber_price:
+    def __init__(self):
+        self.account = tibber.Account(tibber_api_key)
+
+    def get_price(self):
+        price_info = self.account.homes[0].current_subscription.price_info
+        if (
+            arrow.now() - arrow.get(price_info.current.starts_at)
+        ).seconds >= 3600:
+            self.account.update()
+        else:
+            return price_info.current.cache
+        price_info = self.account.homes[0].current_subscription.price_info
+        return price_info.current.cache
