06f61ef36ba2b8c9449ed4b159cb59e534e6b345
Jannis Lübbe <jannisluebbe1982@gmail.com>
2024-07-27 20:21:30 +0200

    handling the smart meter data with higher priority than the electricity meter.

--- unknown_consumers.py
+++ unknown_consumers.py
@@ -6,10 +6,9 @@ from config import *
 redis_connection = redis.Redis(decode_responses=True)
 
 
-def collect_meter_power(utc_start: int):
+def collect_meter_power(utc_start: int, key: str = "electricity_meter"):
     _meter_data = [
-        json.loads(_row)
-        for _row in redis_connection.lrange("electricity_meter", 0, -1)[::-1]
+        json.loads(_row) for _row in redis_connection.lrange(key, 0, -1)[::-1]
     ]
     return [
         _row["power"] - _row["powerProduction"]
@@ -19,25 +18,34 @@ def collect_meter_power(utc_start: int):
 
 
 def process_unknown_consumers(report: dict) -> None:
-    _recent_meter_power = collect_meter_power(
-        report["utc"] - unknown_consumers_interval
+    _recent_electricity_meter_power = collect_meter_power(
+        report["utc"] - unknown_consumers_interval, "electricity_meter"
     )
-    if len(_recent_meter_power) > 0:
+    _recent_smart_meter_power = collect_meter_power(
+        report["utc"] - unknown_consumers_interval, "smart_meter"
+    )
+    # ensure that recent consumption data is available
+    if len(_recent_electricity_meter_power) > 0:
         _meter_data = json.loads(
             redis_connection.lrange("electricity_meter", 0, 0)[0]
         )
         report["meter_consumed"] = _meter_data["lastMeterConsumption"]
         report["meter_produced"] = _meter_data["lastMeterProduction"]
-        _meter_power = np.median(_recent_meter_power)
-        if report.get("battery_power") is None:
-            _battery_power = 0
-        else:
-            _battery_power = report["battery_power"]
-        report["unknown_consumers_power"] = (
-            _meter_power
-            - report["consumer_power"]
-            + report["producer_power"]
-            + _battery_power
-        )
+    # prefer smart meter data over electricity meter readout
+    if len(_recent_smart_meter_power) > 0:
+        _meter_power = np.median(_recent_smart_meter_power)
+    elif len(_recent_electricity_meter_power) > 0:
+        _meter_power = np.median(_recent_electricity_meter_power)
     else:
         report["unknown_consumers_power"] = 0
+        return
+    if report.get("battery_power") is None:
+        _battery_power = 0
+    else:
+        _battery_power = report["battery_power"]
+    report["unknown_consumers_power"] = (
+        _meter_power
+        - report["consumer_power"]
+        + report["producer_power"]
+        + _battery_power
+    )
